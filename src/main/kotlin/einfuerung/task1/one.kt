0e79c2835c427423b969bac6c5e4f82bcd612058
Marco Matteo <marco.matteo@edu.tbz.ch>
2026-05-11 10:57:14 +0200

    init + einfuerung ready for review

---
+++ src/main/kotlin/einfuerung/task1/one.kt
@@ -0,0 +1,25 @@
+package einfuerung.task1
+
+fun calculateScore(word: String): Int {
+    var score = 0
+    for (c in word.toCharArray()) {
+        if (c != 'a') {
+            score++
+        }
+    }
+    return score
+}
+
+fun wordScore(word: String): Int {
+    return word.count {
+        it != 'a'
+    }
+    // word.filter { c -> c != 'a' }.length
+}
+
+fun main() {
+    println(calculateScore("imperative"))
+    println(calculateScore("no"))
+    println(wordScore("declarative"))
+    println(wordScore("yes"))
+}
---
+++ src/main/kotlin/einfuerung/task1/three.kt
@@ -0,0 +1,13 @@
+package einfuerung.task1
+
+fun calculateTip(people: List<String>): Int {
+    return when (people.size) {
+        0 -> 0
+        in 1..5 -> 10
+        else -> 20
+    }
+}
+
+fun main() {
+    println(calculateTip(listOf("Fritz", "Franz", "Hugo", "Erwin", "Markus", "Martin", "Heiri")))
+}
